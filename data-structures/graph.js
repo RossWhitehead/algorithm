cea12ccd12808df6ad2fd9abc24f32c67a3f0bc9
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2019-07-26 09:52:32 +0100

    Add graph dfs

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -16,17 +16,16 @@ class Graph {
     addEdge(vertex1, vertex2) {
         // uni-directional
         this.adjList.get(vertex1).push(vertex2)
-        this.adjList.get(vertex2).push(vertex1)
     }
 
-    bft(vertex) {
+    bfs(vertex) {
         const results = []
         const visited = new Set()
-        this._traverse(vertex, results, visited)
+        this._bfTraverse(vertex, results, visited)
         return results
     }
 
-    _traverse(vertex, results, visited) {
+    _bfTraverse(vertex, results, visited) {
         if (!results.includes(vertex)) {
             results.push(vertex)
         }
@@ -40,7 +39,29 @@ class Graph {
             })
             this.adjList.get(vertex).forEach((edge) => {
                 if (!visited.has(edge)) {
-                    this._traverse(edge, results, visited)
+                    this._bfTraverse(edge, results, visited)
+                }
+            })
+        }
+    }
+
+    dfs(vertex) {
+        const results = []
+        const visited = new Set()
+        this._dfTraverse(vertex, results, visited)
+        return results
+    }
+
+    _dfTraverse(vertex, results, visited) {
+        if (!results.includes(vertex)) {
+            results.push(vertex)
+        }
+        if (!visited.has(vertex)) {
+            visited.add(vertex)
+
+            this.adjList.get(vertex).forEach((edge) => {
+                if (!visited.has(edge)) {
+                    this._dfTraverse(edge, results, visited)
                 }
             })
         }
--- tests/data-structures-tests/graph-tests.spec.js
+++ tests/data-structures-tests/graph-tests.spec.js
@@ -13,7 +13,7 @@ describe('Graph', function () {
         g.addEdge('E', 'F') 
         g.addEdge('E', 'C') 
         g.addEdge('C', 'F') 
-        expected = 'A -> B D E\nB -> A C\nC -> B E F\nD -> A E\nE -> A D F C\nF -> E C\n'
+        expected = 'A -> B D E\nB -> C\nC -> F\nD -> E\nE -> F C\nF ->\n'
         assert.equal(expected, g.toString())
     })
     it('bfs returns valid result', function () {
@@ -28,6 +28,27 @@ describe('Graph', function () {
         g.addEdge('E', 'C') 
         g.addEdge('C', 'F') 
         expected = ['A', 'B', 'D', 'E', 'C', 'F']
-        assert.deepEqual(expected, g.bft('A'))
+        assert.deepEqual(expected, g.bfs('A'))
+    })
+    it('dfs returns valid result', function () {
+        const g =  new Graph()
+        g.addVertices(['A', 'B', 'C', 'D', 'E', 'F'])
+        g.addEdge('A', 'B')
+        g.addEdge('A', 'D') 
+        g.addEdge('A', 'E') 
+        g.addEdge('B', 'C') 
+        g.addEdge('D', 'E') 
+        g.addEdge('E', 'F') 
+        g.addEdge('E', 'C') 
+        g.addEdge('C', 'F') 
+        // Note. unidirectional
+        // A -> B D E
+        // B -> C
+        // C -> F
+        // D -> E
+        // E -> F C
+        // F -> 
+        expected = ['A', 'B', 'C', 'F', 'D', 'E']
+        assert.deepEqual(expected, g.dfs('A'))
     })
 })
